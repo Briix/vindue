50bad297c63084f36dbb33604faa2bb88a2f6ce8
Simon Brix <s.brix@eslgaming.com>
2020-07-12 21:37:05 +0200

    move index styles to index file

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,16 @@ import { SiteContainer } from './index.styles'
 import Sidebar from '../components/Sidebar'
 import Browser from '../components/Browser'
 import ImageUpload from '../components/ImageUpload'
+import styled from 'styled-components'
+
+export const SiteContainer = styled.main`
+  display: flex;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  padding-right: 300px;
+`
 
 export default function Home() {
   const [image, setImage] = useState({})
